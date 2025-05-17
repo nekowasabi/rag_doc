cfde03995ba0d5b1c5d4bcda707c669f2772cf5c
nekowasabi <nolifeking00@gmail.com>
2025-05-17 16:06:13 +0900

    feat: 依存関係を更新

--- deps.ts
+++ deps.ts
@@ -1,4 +1,7 @@
-export { Database } from "https://deno.land/x/sqlite3@0.8.0/mod.ts";
-export { MeCab } from "https://deno.land/x/deno_mecab@v1.2.3/mod.ts";
+export { Database } from "https://deno.land/x/sqlite3@0.12.0/mod.ts";
+export { default as MeCab } from "https://deno.land/x/deno_mecab@v1.2.3/mod.ts";
 export { OpenAI } from "https://deno.land/x/openai@v4.69.0/mod.ts";
-export { TextCleaner, SplitTokenizer } from "https://deno.land/x/vectorizer@v0.7.5/mod.ts";
+export {
+  SplitTokenizer,
+  TextCleaner,
+} from "https://deno.land/x/vectorizer@v0.7.5/mod.ts";
